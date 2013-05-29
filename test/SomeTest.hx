27d31ec28a9fe3dadf7d605574e4cbf727dffed7
Aleksey Fomkin <aleksey.fomkin@gmail.com>
2013-05-29 14:28:52 +0400

    maven integration

---
+++ test/SomeTest.hx
@@ -0,0 +1,13 @@
+import haxe.unit.TestCase;
+
+class SomeTest extends TestCase {
+
+    public function new() {
+        super();
+    }
+
+    public function testSomethong() {
+	assertEquals(1, 1);
+    }
+
+}
