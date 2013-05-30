cd9ac0136af094a2d4ca28ca0ebe760f0ea961ad
Sergey Egorov <bsideup@gmail.com>
2013-05-30 21:00:40 +0300

    Basic test

--- test/hxrm/tests/parser/MXMLParserTests.hx
+++ test/hxrm/tests/parser/MXMLParserTests.hx
@@ -8,15 +8,23 @@ class MXMLParserTests extends TestCase {
 	}
 
 	public function testSimpleObject() {
+		checkSimpleObjectWithNamespacePrefix('<String xmlns="my.package.*"/>', "my.package.*", "String", "*");
+		checkSimpleObjectWithNamespacePrefix('<ns:String xmlns:ns="*"/>', "*", "String", "ns");
+	}
+
+	function checkSimpleObjectWithNamespacePrefix(value : String, expectedPackage : String, expectedName : String, expectedNamespace : String) {
 		var mxmlParser : MXMLParser = new MXMLParser();
 		
-		var mxmlNode : MXMLNode = mxmlParser.parse('<String xmlns="*"/>');
-		
-		assertEquals("*", mxmlNode.name.namespace);
-		assertEquals("String", mxmlNode.name.localPart);
+		var mxmlNode : MXMLNode = mxmlParser.parse(value);
+
+		assertEquals(expectedNamespace, mxmlNode.name.namespace);
+		assertEquals(expectedName, mxmlNode.name.localPart);
 		assertEquals(0, mxmlNode.children.length);
 		assertEquals(null, mxmlNode.parentNode);
 		assertEquals(0, mxmlNode.typeParams.length);
 		assertFalse(mxmlNode.values.iterator().hasNext());
+
+		assertTrue(mxmlNode.namespaces.exists(expectedNamespace));
+		assertEquals(expectedPackage, mxmlNode.namespaces.get(expectedNamespace));
 	}
 }
