b5512fdb7471e38e41f72f0c68b7ceebed6463da
Sergey Egorov <bsideup@gmail.com>
2013-06-01 22:48:29 +0300

    much better Type pretty print!

--- src/hxrm/utils/TypeUtils.hx
+++ src/hxrm/utils/TypeUtils.hx
@@ -1,11 +1,57 @@
 package hxrm.utils;
 
 class TypeUtils {
-	static var r = ~/(.*?=>.*?), /g; // g : replace all instances
 
 	public static function prettyPrintType(type : Dynamic) : Void {
-		var scopeAsString : String = Std.string(type);
-		scopeAsString = r.replace(scopeAsString, "$1,\n\t");
-		trace(scopeAsString.split("{").join("{\n\t").split("}").join("\n}\n"));
+		var result : String = "";
+		var i : Int = 0;
+		var splitted = Std.string(type);
+		
+		for(index  in 0...splitted.length) {
+			var s = splitted.charAt(index);
+			if(s == "{") {
+				i++;
+				result += "{\n" + indent(i);
+			} else if(s == "}") {
+				i--;
+				result += "\n" + indent(i) + "}";
+			} else if(s == ",") {
+				
+				var nextIndex = index;
+				var found = false;
+				while(true) {
+					nextIndex++;
+					if(nextIndex >= splitted.length) {
+						break;
+					}
+					var nextS = splitted.charAt(nextIndex);
+					
+					if(nextS == ",") {
+						break;
+					} else if(nextS == "=" && (nextIndex + 1) < splitted.length &&  splitted.charAt(nextIndex + 1) == ">") {
+						found = true;
+						break;
+					}
+				}
+				if(found) {
+					result += ",\n" + indent(i);
+				} else {
+					result += ",";
+				}
+				
+			} else if(s == "\n") {
+				result += "\n" + indent(i);
+			}
+			else {
+				result += s;
+			}
+		}
+		
+		trace(result);
+	}
+
+	static inline function indent(indentLevel : Int) : String {
+		var glue : String = "  ";
+		return StringTools.rpad("", glue, indentLevel * glue.length);
 	}
 }
