cc427360a9cfc9a71b17e3d5b8f0c9a984fbfcda
Sergey Egorov <bsideup@gmail.com>
2013-07-13 22:49:31 +0300

    interface IHxmrExtension extends IGeneratorExtension extends INodeAnalyzerExtension

--- src/main/haxe/hxrm/extensions/base/IHxmrExtension.hx
+++ src/main/haxe/hxrm/extensions/base/IHxmrExtension.hx
@@ -1,12 +1,5 @@
 package hxrm.extensions.base;
-import hxrm.generator.GeneratorScope;
-import hxrm.analyzer.NodeScope;
-interface IHxmrExtension {
 
-    // return true if it needs one more iteration
-    function analyze(context : HxmrContext, scope : NodeScope) : Bool;
-
-    // return true if it needs one more iteration
-    function generate(context:HxmrContext, scope:GeneratorScope) : Bool;
+interface IHxmrExtension extends IGeneratorExtension extends INodeAnalyzerExtension {
 
 }
